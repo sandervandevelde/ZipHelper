822b6b29fece6c4a2d3ec9f3e8e01754ec975922
Sander van de Velde <svelde@gmail.com>
2021-07-05 16:32:32 +0200

    gzip support added

---
+++ ZipHelperLib/GZipHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace ZipHelperLib
+{
+    public class GZipHelper
+    {
+        public static byte[] Zip(byte[] byteArray)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+                {
+                    using (var streamWriter = new BufferedStream(gzipStream)) // encoding is not relevant
+                    {
+                        streamWriter.Write(byteArray, 0, byteArray.Length);
+                    }
+                }
+
+                return memoryStream.ToArray();
+            }
+        }
+
+        public static byte[] Unzip(byte[] byteArray)
+        {
+            using (var zippedMemoryStream = new MemoryStream(byteArray))
+            {
+                using (var unZippedMemoryStream = new MemoryStream())
+                {
+                    using (var unZippedBufferedStream = new BufferedStream(new GZipStream(zippedMemoryStream, CompressionMode.Decompress)))
+                    {
+                        unZippedBufferedStream.CopyTo(unZippedMemoryStream);
+                    }
+                    return unZippedMemoryStream.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ ZipHelperUnitTest/GZipHelperTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZipHelperLib;
+
+namespace ZipHelperUnitTest
+{
+    [TestClass]
+    public class GZipHelperTest
+    {
+        [TestMethod]
+        public void TestZipUnzipSucceeds()
+        {
+            //// ARRANGE
+
+            var source = new byte[992];
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                source[i] = 42;
+            }
+
+            //// ACT
+
+            var zippedSource = ZipHelper.Zip(source);
+
+            var unzipped = ZipHelper.UnzipByteArray(zippedSource);
+
+            //// ASSERT
+
+            Assert.AreEqual(992, unzipped.Length);
+
+            for (int i = 0; i < 992; i++)
+            {
+                Assert.AreEqual(source[i], unzipped[i]);
+            }
+        }
+    }
+}
